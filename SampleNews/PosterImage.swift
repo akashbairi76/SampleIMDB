94209f45fe934d4ad67b5f58a8d8b95d191fcc5e
akash <akash.bairi@housing.com>
2023-10-23 08:22:03 +0530

    added image path so that user can see poster image

---
+++ SampleNews/PosterImage.swift
@@ -0,0 +1,48 @@
+//
+//  PosterImage.swift
+//  SampleNews
+//
+//  Created by Bairi Akash on 23/10/23.
+//
+
+import SwiftUI
+
+struct PosterImage: View {
+    var temporaryImage: Image = Image("photo")
+    var edgeRadius: CGFloat = 20
+    var imageFileName: String?
+    var imagePath: String? {
+        guard let imageFileName = imageFileName else {
+            return nil
+        }
+        return "https://image.tmdb.org/t/p/w500/" + imageFileName
+    }
+    
+    
+    var body: some View {
+        Group {
+            if let imagePath = imagePath {
+                AsyncImage(url: URL(string: imagePath)) { image in
+                    image
+                        .resizable()
+                        .clipShape(RoundedRectangle(cornerRadius: edgeRadius))
+                } placeholder: {
+                    
+                    temporaryImage
+                        .resizable()
+                        .clipShape(RoundedRectangle(cornerRadius: edgeRadius))
+                }
+            }else {
+                temporaryImage
+                    .resizable()
+                    .clipShape(RoundedRectangle(cornerRadius: edgeRadius))
+            }
+        }
+    }
+}
+
+struct PosterImage_Previews: PreviewProvider {
+    static var previews: some View {
+        PosterImage()
+    }
+}
--- SampleNews/View/ContentView.swift
+++ SampleNews/View/ContentView.swift
@@ -8,15 +8,22 @@ struct ContentView: View {
     var body: some View {
         NavigationView {
             List(networkManager.posts, id: \.id) { post in
-                    VStack {
+                
+                HStack{
+                    PosterImage(edgeRadius: 8,imageFileName: post.poster_path)
+                        .frame(width: 96, height: 120)
+                    VStack( alignment: .leading) {
+                       
                         Text(post.title!).font(.title2)
-                            .foregroundColor(Color.gray.opacity(0.8))
                             .bold()
                             .padding(EdgeInsets(top: 0, leading: 0, bottom: 8, trailing: 0))
-                        
-                        Text(post.overview!) //.foregroundColor(Color.black.opacity(0.5))
+                       
+                        Text(post.overview!).lineLimit(3)
+                        Spacer()
                     }
-                
+                }
+                    
+                            
             }
             
         }
